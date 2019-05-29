3c8da6e86fb95413283143fff2848f8fc3322c4c
sushil pathariya <sushilpathariya@gmail.com>
2019-05-29 16:55:40 +0530

    Updates

---
+++ precache-manifest.32f6b68ec4be94b6a4cef93508a31bc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2b6391f4e77e8b602fe7909dff6b4e",
+    "url": "/sushil1983/react-temperature-app/index.html"
+  },
+  {
+    "revision": "d6199272d18e7a305896",
+    "url": "/sushil1983/react-temperature-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3aa10ca9e7dda998ee7f",
+    "url": "/sushil1983/react-temperature-app/static/css/main.a2a003c9.chunk.css"
+  },
+  {
+    "revision": "d6199272d18e7a305896",
+    "url": "/sushil1983/react-temperature-app/static/js/2.daa9d9db.chunk.js"
+  },
+  {
+    "revision": "3aa10ca9e7dda998ee7f",
+    "url": "/sushil1983/react-temperature-app/static/js/main.796ea9fe.chunk.js"
+  },
+  {
+    "revision": "c0c43df12464813e4876",
+    "url": "/sushil1983/react-temperature-app/static/js/runtime~main.a9bdd3f7.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/sushil1983/react-temperature-app/static/media/bg.b6796c73.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sushil1983/react-temperature-app/precache-manifest.32f6b68ec4be94b6a4cef93508a31bc1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sushil1983/react-temperature-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
